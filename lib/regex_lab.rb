ae2442e60891da4e6dcff99d37f5405bfb2d272b
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-01 17:59:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  true if word.match(/[aeiou]/) else false
+  if word.match(/[aeiou]/)
+    true 
+  else 
+    false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
