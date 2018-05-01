8b4b4e4da330c0bdfb3c90304531f274d23e6d26
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-01 18:29:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  true if text.match(/^[A-Z].*\W$/)
+  text.match(/^[A-Z].*\W$/)? true: false 
 end
 
 def valid_phone_number?(phone)
